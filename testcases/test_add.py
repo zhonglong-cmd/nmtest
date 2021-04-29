c16f9e0a0a20659ef973fa492baf49cb8e38a061
钟龙 <67143282+zhonglong-cmd@users.noreply.github.com>
2021-04-29 11:18:51 +0800

    Update test_add.py

--- testcases/test_add.py
+++ testcases/test_add.py
@@ -1,11 +1,4 @@
-"""
-============================
-Author:柠檬班-木森
-Time:2020/2/28   21:21
-E-mail:3247119728@qq.com
-Company:湖南零檬信息技术有限公司
-============================
-"""
+
 import os
 import unittest
 import jsonpath
